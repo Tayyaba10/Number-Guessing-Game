04ed0ed8ca3ec16673a835d2e240d2b1a8ad2bb9
Tayyaba10 <159796040+Tayyaba10@users.noreply.github.com>
2024-03-24 21:34:49 +0500

    Number guessing game
    This is a number guessing game

---
+++ index.ts
@@ -0,0 +1,18 @@
+#! usr/bin/env/ node
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random());
+const answer = await inquirer.prompt([
+  
+    {
+      name: "userGuessNumber",
+      type: "number",
+      message: "please guess a number between 5-10:",
+    },
+  ],
+);
+
+if (answer.userGuessNumber === randomNumber){
+    console.log("Congratulation! you guess a right number");
+}
+else {(console.log("you guess a wrong number.Please Try Again"))};
